7a93ab5b88a355342ba146b32930ee9917c9230e
RAVEN DELANEY <ravend17@gmail.com>
2019-12-05 15:41:02 -0800

    classes formed, tests started

--- garage_band.py
+++ garage_band.py
@@ -12,6 +12,15 @@ class Band:
 
     @classmethod
     def create_from_data(cls, data):
+        """Create a new band with members Musicians with given data.
+        Data in a string in form
+        Each line has 'name, instrument'
+        First is band name, rest members
+
+
+        Arguments:
+            data{[type]} -- [description]
+        """
         lines = data.split('\n')
         name_line = lines[0]
         line_parts = name_line.split('.')
@@ -33,8 +42,44 @@ class Musician:
     def to_list(cls):
         return cls.member_list
 
-    def get_instrument
-        return self.instrument
+    def get_instrument():
+        return Musician.instrument
 
-    def play_solo
-        return self.name
+    def play_solo():
+        return Musician.name
+
+
+class Vocals:
+
+    def __init__(self, name)
+    super().__init__(name, 'vocalist')
+
+    def play_solo(self):
+        return 'vocal solo'
+
+
+class Guitar:
+
+    def __init__(self, name)
+    super().__init__(name, 'guitarist')
+
+    def play_solo(self):
+        return 'guitar solo'
+
+
+class Bass:
+
+    def __init__(self, name)
+    super().__init__(name, 'bassist')
+
+    def play_solo(self):
+        return 'bass solo'
+
+
+class Drums:
+
+    def __init__(self, name)
+    super().__init__(name, 'drummer')
+
+    def play_solo(self):
+        return 'drum solo'
